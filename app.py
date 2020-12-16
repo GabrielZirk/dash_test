92b64c0dcddbd67228e24204045e2f116abbd39e
Gabriel Zirkovits <gabriel.zirkovits@edu.campus02.at>
2020-12-16 19:47:12 +0100

    added new graphs

--- app.py
+++ app.py
@@ -28,7 +28,6 @@ covf_delta["Datum"] = covf_delta.Datum.str[0:10]
 fig_timeline = px.bar(covf_delta,
                       x="Datum",
                       y = "DeltaAnzahlVortag",
-                      title = "Neuinfektionen",
                      color = "DeltaAnzahlVortag", orientation = "v",
              color_continuous_scale = "inferno", labels = dict(Datum = "Datum", DeltaAnzahlVortag = "Neuinfektionen/Tag", sex = "Neuinfektionen/Tag"))
 
@@ -36,12 +35,88 @@ fig_timeline.update_layout(xaxis = dict(
         tickmode = 'linear',
         dtick = 10
     ))
+########################
+z = urlopen("https://covid19-dashboard.ages.at/data/data.zip")
+covidzip = ZipFile(BytesIO(z.read())).extract("CovidFaelle_Altersgruppe.csv")
+covf_ag = pd.read_csv(covidzip, delimiter=";")
+
+covf_ag_oe = covf_ag[covf_ag["Bundesland"] == "Österreich"]
+
+fig_agegroup_mw_cases = px.bar(covf_ag_oe,
+                        x = "Altersgruppe",
+                        y = "Anzahl",
+                        color = "Geschlecht",
+                        color_discrete_sequence=px.colors.qualitative.Pastel,
+                        template = "ggplot2")
+
+fig_agegroup_mw_cases.update_layout(title = {"xanchor" : "left"})
+###################################
+fig_agegroup_mw_death = px.bar(covf_ag_oe,
+                        x = "Altersgruppe",
+                        y = "AnzahlTot",
+                        color = "Geschlecht",
+                        barmode = "group",
+                        color_discrete_sequence=px.colors.qualitative.Pastel,
+                        text = "AnzahlTot",
+                        template = "ggplot2")
+fig_agegroup_mw_death.update_traces(texttemplate = '%{text:.3s}', textposition ='outside')
+fig_agegroup_mw_death.update_layout(uniformtext_minsize = 4, uniformtext_mode ='hide')
+####################################
+z = urlopen("https://covid19-dashboard.ages.at/data/data.zip")
+covidzip = ZipFile(BytesIO(z.read())).extract("CovidFaelle_Timeline.csv")
+covf_epik = pd.read_csv(covidzip, delimiter=";")
+
+covf_epik_at = covf_epik[covf_epik["Bundesland"] == "Österreich"]
+
+covf_epik_at["Time"] = covf_epik_at.Time.str[0:10]
+
+fig_covf_epik = go.Figure()
+
+fig_covf_epik.add_trace(go.Scatter(x = covf_epik_at["Time"],
+                                  y = covf_epik_at["AnzahlFaelleSum"],
+                                  mode = "lines",
+                                   name = "Kumm. Anzahl Fälle",
+                                  fill = "tozeroy",
+                                   marker = dict(color = "LightSkyBlue")))
+
+fig_covf_epik.add_trace(go.Scatter(x = covf_epik_at["Time"],
+                                  y = covf_epik_at["AnzahlGeheiltSum"],
+                                  mode = "lines",
+                                   name = "Kumm. Anzahl Geheilte",
+                                  fill='tozeroy',
+                                  marker = dict(color = "green")))
+
+fig_covf_epik.add_trace(go.Scatter(x = covf_epik_at["Time"],
+                                  y = covf_epik_at["AnzahlTotSum"],
+                                  mode = "lines",
+                                   name = "Kumm. Anzahl Tote",
+                                  fill = "tozeroy",
+                                  marker = dict(color = "red")))
+
+fig_covf_epik.update_layout(xaxis = dict( tickmode = 'linear',
+                                         dtick = 10,
+                                        tickangle=45),
+                                        template = "ggplot2")
 
 
-app.layout = html.Div([
-    html.H2('Data Science Rox :)'),
-    dcc.Graph(id = "covid_dash",
-              figure = fig_timeline)
+app.layout = html.Div(children=[
+    html.H1('Data Science Rox :)'),
+    html.H3(children = """Tägliche Neuinfektionen"""),
+    dcc.Graph(id = "Timeline Cases",
+              figure = fig_timeline),
+
+    html.H3(children = """Anzahl Erkrankter nach Altersgruppe"""),
+    dcc.Graph(id = "Cases by agegroup",
+              figure = fig_agegroup_mw_cases),
+
+    html.H3(children = """Anzahl Tote nach Altersgruppe"""),
+    dcc.Graph(id = "Deaths by agegroup",
+              figure = fig_agegroup_mw_death),
+
+    html.H3(children = """Epikurven"""),
+    dcc.Graph(id = "epicurve",
+              figure = fig_covf_epik)
+
 ])
 
 
